00e7236c053d3d36cfc207ba2ddef3e7d76edff1
Alexander Farrell <alexander.j.farrell@outlook.com>
2021-09-30 21:43:41 -0700

    Added a template as well as an index screen to get to the scheduling page.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('template', { title: 'Programming Crash Courses', content: 'index.ejs' });
+});
+router.get('/embark', function(req, res, next) {
+  res.render('template', { title: 'Programming Crash Courses', content: 'trial.ejs' });
+});
+router.get('/schedule', function(req, res, next) {
+  res.render('template', { title: 'Programming Crash Courses', content: 'schedule.ejs' });
+});
+router.get('/one-hour', function(req, res, next) {
+  res.render('template', { title: 'Programming Crash Courses', content: 'one-hour.ejs' });
+});
+router.get('/two-hours', function(req, res, next) {
+  res.render('template', { title: 'Programming Crash Courses', content: 'two-hours.ejs' });
 });
 
 module.exports = router;
