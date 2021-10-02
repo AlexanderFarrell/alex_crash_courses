11c001537f2c034e16dcbe5ada67c33262f79fd4
Alexander Farrell <alexander.j.farrell@outlook.com>
2021-10-01 23:50:04 -0700

    Removed Trial, cleaned up and streamlined website a bit. Readied it for Facebook.

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
+function add_page(router, path, page_title, page_content){
+  router.get(path, (req, res) => {
+    res.render('template', { title: page_title + " - Alexander Farrell", content: page_content})
+  })
+}
+
+add_page(router, '/', 'Home', 'index.ejs');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('template', { title: 'Programming Crash Courses', content: 'index.ejs' });
-});
-router.get('/embark', function(req, res, next) {
-  res.render('template', { title: 'Programming Crash Courses', content: 'trial.ejs' });
-});
-router.get('/schedule', function(req, res, next) {
-  res.render('template', { title: 'Programming Crash Courses', content: 'schedule.ejs' });
-});
-router.get('/one-hour', function(req, res, next) {
-  res.render('template', { title: 'Programming Crash Courses', content: 'one-hour.ejs' });
-});
-router.get('/two-hours', function(req, res, next) {
-  res.render('template', { title: 'Programming Crash Courses', content: 'two-hours.ejs' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('template', { title: 'Programming Crash Courses', content: 'index.ejs' });
+// });
+// router.get('/embark', function(req, res, next) {
+//   res.render('template', { title: 'Programming Crash Courses', content: 'trial.ejs' });
+// });
+// router.get('/schedule', function(req, res, next) {
+//   res.render('template', { title: 'Programming Crash Courses', content: 'schedule.ejs' });
+// });
+// router.get('/one-hour', function(req, res, next) {
+//   res.render('template', { title: 'Programming Crash Courses', content: 'one-hour.ejs' });
+// });
+// router.get('/two-hours', function(req, res, next) {
+//   res.render('template', { title: 'Programming Crash Courses', content: 'two-hours.ejs' });
+// });
 
 module.exports = router;
